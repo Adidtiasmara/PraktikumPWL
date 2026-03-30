e3283432d87ee404e2c6d6f2ff24edd821a9681a
Muhammad Firman Aditiasmara <mfirmanadid@gmail.com>
2026-03-30 16:27:02 +0700

    upd 5.4

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -20,8 +20,8 @@ class PostForm
         return $schema
             ->components([
                 //
-                TextInput::make('title')->required(),
-                TextInput::make('slug')->required(),
+                TextInput::make('title')->required()->minLength(5),
+                TextInput::make('slug')->required()->unique(ignoreRecord: true),
                 Select::make("category_id")
                     ->relationship("category", "name")
                     ->preload()
